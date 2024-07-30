500d74818bb1547d2ebd0aeefbd6c5ea9db08bd3
Ashraf-Hossain <ashrafhossainsohan@gmail.com>
2024-07-30 19:38:30 +0600

    Array Resize Added

---
+++ DSA/src/MoveZero.java
@@ -0,0 +1,24 @@
+public class MoveZero {
+    static void MoveThezero(int[] arr){
+        int j = 0;
+        for (int i = 0 ; i < arr.length ;i++){
+            if (arr[i] != 0 && arr[j] == 0){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            if (arr[j] != 0){
+                j++;
+            }
+        }
+    }
+    public static void main(String[] args){
+
+        int[] moveTheValue ={15,63,0,2,0,25,0,59,36,0,69};
+        MoveThezero(moveTheValue);
+
+        for (int j : moveTheValue) {
+            System.out.print(j + " ");
+        }
+    }
+}
---
+++ DSA/src/ResizeArray.java
@@ -0,0 +1,17 @@
+public class ResizeArray {
+    static int[] ResizeTheArray(int[] GivenArray , int Capacity){
+        int[] temp = new int[Capacity];
+        for (int i = 0 ; i < GivenArray.length ; i++ ) {
+            temp[i] = GivenArray[i];
+        }
+        return temp;
+    }
+    public static void main (String[] args){
+        int[] needToResize = {10,20,30,40};
+        System.out.println(needToResize.length);
+
+        needToResize = ResizeTheArray(needToResize,10);
+
+        System.out.println(needToResize.length);
+    }
+}
