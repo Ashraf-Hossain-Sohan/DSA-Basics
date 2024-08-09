17f33a0e2b9fe52f19117d7264ba1cf500efc6a9
Ashraf-Hossain <ashrafhossainsohan@gmail.com>
2024-08-09 12:03:42 +0600

    Array Find Missing Value Added

---
+++ DSA/src/MissingNumber.java
@@ -0,0 +1,15 @@
+public class MissingNumber {
+    static void MissingValue(int[] FindTheNumber){
+        int n = FindTheNumber.length + 1;
+        int sum = n * (n+1)/2;
+        for(int num : FindTheNumber){
+            sum -= num;
+        }
+        System.out.println(sum);
+    }
+    public static void main (String[] args){
+        int[] FindTheNumber ={1,2,3,4,6,7,8,9};
+        MissingValue(FindTheNumber);
+
+    }
+}
